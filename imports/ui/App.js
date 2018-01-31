b6cb8ea5c98f3804361854a37b907c471a6f0adb
Esteban Ortiz <ematiasortiz@gmail.com>
2018-01-31 17:00:46 -0300

    check "ocultar tareas completadas" (sigue sin modificar)

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -8,6 +8,15 @@ import Task from './Task.js';
  
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+ 
+    this.state = {
+      hideCompleted: false,
+    };
+  }
+
   handleSubmit(event) {
     event.preventDefault();
  
@@ -23,8 +32,18 @@ class App extends Component {
     ReactDOM.findDOMNode(this.refs.textInput).value = '';
   }
 
+  toggleHideCompleted() {
+    this.setState({
+      hideCompleted: !this.state.hideCompleted,
+    });
+  }
+
   renderTasks() {
-    return this.props.tasks.map((task) => (
+    let filteredTasks = this.props.tasks;
+    if (this.state.hideCompleted) {
+      filteredTasks = filteredTasks.filter(task => !task.checked);
+    }
+    return filteredTasks.map((task) => (
       <Task key={task._id} task={task} />
     ));
   }
@@ -33,8 +52,17 @@ class App extends Component {
      return (
        <div className="container">
          <header>
-           <h1>Lista de tareas</h1>
+          <h1>Lista tareas ({this.props.incompleteCount})</h1>
 
+          <label className="hide-completed">
+            <input
+              type="checkbox"
+              readOnly
+              checked={this.state.hideCompleted}
+              onClick={this.toggleHideCompleted.bind(this)}
+            />
+            Ocultar tareas completadas
+          </label>
 
           <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
             <input
@@ -56,6 +84,7 @@ class App extends Component {
 export default withTracker(() => {
   return {
     tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
+    incompleteCount: Tasks.find({ checked: { $ne: true } }).count(),
   };
 })(App);
 
