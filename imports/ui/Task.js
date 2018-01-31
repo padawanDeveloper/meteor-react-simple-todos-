4eec036cfcbbd87a10e84a92b0b34b641df84ca5
Esteban Ortiz <ematiasortiz@gmail.com>
2018-01-31 15:31:07 -0300

    update and remove (no funciona el update)

--- imports/ui/Task.js
+++ imports/ui/Task.js
@@ -1,38 +1,39 @@
 import React, { Component } from 'react';
-import {Tasks} from '../api/tasks.js';
-
  
-// Task component - represents a single todo item
+import { Tasks } from '../api/tasks.js';
+ 
+
 export default class Task extends Component {
-	toggleChecked(){
-		Tasks.update(this.props.task._id,{
-			$set: {checked: !this.props.task.checked},
-		});
-	}
+  
+  toggleChecked() { 
+    Tasks.update(this.props.task._id, {
+      $set: { checked: !this.props.task.checked },
+    });
+  }
+ 
+  deleteThisTask() {
+    Tasks.remove(this.props.task._id);
+  }
+ 
+  render() {
 
-	deleteThisTask() {
-	    Tasks.remove(this.props.task._id);
-	}
-
-	render() {
-
-		const taskClassName = this.props.task.checked ? 'checked' : '';
-
-	    return (
-		    <li className={taskClassName}>
-	        	<button className="delete" onClick={this.deleteThisTask.bind(this)}>
-	          		&times;
-	       		</button>
-	 
-	        	<input
-		          type="checkbox"
-		          readOnly
-		          checked={!!this.props.task.checked}
-		          onClick={this.toggleChecked.bind(this)}
-		        />
-	 
-		        <span className="text">{this.props.task.text}</span>
-		    </li>
-	    );
-	}
+    const taskClassName = this.props.task.checked ? 'checked' : '';
+ 
+    return (
+      <li className={taskClassName}>
+        <button className="delete" onClick={this.deleteThisTask.bind(this)}>
+          &times;
+        </button>
+ 
+        <input
+          type="checkbox"
+          readOnly
+          checked={!!this.props.task.checked}
+          onClick={this.toggleChecked.bind(this)}
+        />
+ 
+        <span className="text">{this.props.task.text}</span>
+      </li>
+    );
+  }
 }
